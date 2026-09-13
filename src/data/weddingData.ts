313d6fdd028419655b7e07f70f0d7bfc82fa52b4
yash3vedi <trivediyash148@gmail.com>
2026-09-14 00:45:31 +0530

    Add files via upload

---
+++ src/data/weddingData.ts
@@ -0,0 +1,22 @@
+export const weddingData = {
+  couple: { groom: 'Yash', bride: 'Pranshu', date: '09 December 2026', timestamp: '2026-12-09T19:00:00+05:30' },
+  families: 'With the blessings of our families',
+  venue: 'The Grand Courtyard, Jaipur, Rajasthan',
+  contact: '+91 98765 43210',
+  hashtag: '#YashWedsPranshu',
+  videoUrl: 'https://www.youtube.com/embed/Scxs7L0vhZ4',
+  events: [
+    { name: 'Ganesh Pujan', date: '08 December', time: '10:00 AM', note: 'An auspicious beginning' },
+    { name: 'Haldi Carnival', date: '08 December', time: '2:00 PM', note: 'Sunshine, laughter & turmeric' },
+    { name: 'Sangeet Night', date: '08 December', time: '7:00 PM', note: 'An evening set to melody' },
+    { name: 'Engagement', date: '09 December', time: '11:00 AM', note: 'A promise, made official' },
+    { name: 'Baraat', date: '09 December', time: '3:00 PM', note: 'Let the celebrations arrive' },
+    { name: 'Reception', date: '09 December', time: '7:00 PM', note: 'Dinner, dancing & delight' },
+    { name: 'Phere', date: '10 December', time: '2:00 AM', note: 'Seven sacred vows' },
+  ],
+  gallery: [
+    'https://images.unsplash.com/photo-1519741497674-611481863552?auto=format&fit=crop&w=1200&q=85',
+    'https://images.unsplash.com/photo-1606800052052-a08af7148866?auto=format&fit=crop&w=1200&q=85',
+    'https://images.unsplash.com/photo-1511285560929-80b456fea0bc?auto=format&fit=crop&w=1200&q=85',
+  ],
+} as const
