060a91a0b7b3c90756930a12e9ca5f88dd8da2e0
yash3vedi <trivediyash148@gmail.com>
2026-09-14 00:44:52 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,2 @@
+import { Hero } from './components/Hero'; import { Invitation, DateReveal, Functions, Couple, Instagram, Video, Countdown, Things, RSVP, Wishes, MusicPlayer, Footer } from './components/Sections';
+export default function App(){return <main><Hero/><Invitation/><DateReveal/><Functions/><Couple/><Instagram/><Video/><Countdown/><Things/><RSVP/><Wishes/><Footer/><MusicPlayer/></main>}
